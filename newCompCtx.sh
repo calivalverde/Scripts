8555ce0d8a3f87180b1dc19ce12bf5dd64bcc9b8
Carlos Valverde <carvalcha@gmail.com>
2022-02-15 12:59:06 -0600

    newCompCtx.sh

--- newCompCtx.sh
+++ newCompCtx.sh
@@ -11,6 +11,9 @@ read new_comp
 echo -n "Context name: "
 read new_ctx_name
 
+echo -n "Context var: "
+read new_ctx_var
+
 echo -n "Context path: "
 read new_ctx_path
 
@@ -21,6 +24,7 @@ wget -O $new_file $template_source
 
 sed -i "s|Comp()|$new_comp()|g" $new_file
 sed -i "s|CompContext|$new_ctx_name|g" $new_file
+sed -i "s|context|$new_ctx_var|g" $new_file
 sed -i "s|ctx_path|$new_ctx_path|g" $new_file
 
 echo $new_ctx_path
